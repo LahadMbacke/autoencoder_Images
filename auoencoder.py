c877a8f1f8c2472dbf284a7b7b3a84a599613739
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-08-08 17:18:24 +0200

    Definition of the RN

--- auoencoder.py
+++ auoencoder.py
@@ -26,7 +26,38 @@ batch_size = 64
 training_dataloader = DataLoader(training_data,batch_size=batch_size)
 test_dataloader = DataLoader(test_data,batch_size=batch_size)
 
-for X,y in test_dataloader:
-    print(f"[N,C,H,W] {X.shape}")
-    print(f"taille , Type {y.shape},{y.dtype}")
-    break
+# for X,y in test_dataloader:
+#     print(f"[N,C,H,W] {X.shape}")
+#     print(f"taille , Type {y.shape},{y.dtype}")
+#     break
+
+# Create Model
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__(self)
+        self.flatten = nn.Flatten
+        #28*28 = 784 ==> 128 ==> 64 ==> 9
+        self.encoder = nn.Sequential(
+            nn.Linear(784, 128),
+            nn.ReLU(),
+            nn.Linear(128,64),
+            nn.ReLU(),
+            nn.Linear(64,9),
+        )
+        self.decoder = nn.Sequential(
+          nn.Linear(9,64),
+          nn.ReLU(),
+          nn.Linear(64,128),
+          nn.ReLU(),
+          nn.Linear(128,784),
+          nn.Sigmoid() 
+        )
+
+    def forward(self,x):
+        x = self.flatten(x)
+        encoder = self.encoder(x)
+        decoder = self.decoder(encoder)
+        return decoder
+    
+

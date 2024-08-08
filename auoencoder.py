91afe2a263aa112ba7d831f9f247fbc8bf6c5c4a
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-08-08 16:49:19 +0200

    Create DataLoader

--- auoencoder.py
+++ auoencoder.py
@@ -5,6 +5,7 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 
+#download training and test data
 
 training_data = datasets.FashionMNIST(
     root="data",
@@ -19,3 +20,13 @@ test_data = datasets.FashionMNIST(
     download=True,
     transform=ToTensor()
 )
+
+# Create dataLoader
+batch_size = 64
+training_dataloader = DataLoader(training_data,batch_size=batch_size)
+test_dataloader = DataLoader(test_data,batch_size=batch_size)
+
+for X,y in test_dataloader:
+    print(f"[N,C,H,W] {X.shape}")
+    print(f"taille , Type {y.shape},{y.dtype}")
+    break
